3c9796fd588be060d23b9be428bb55da51081bba
godblessska <160397010+godblessska@users.noreply.github.com>
2025-10-24 01:02:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def calculate():
     return render_template('index.html', result=result, a=a, b=b, op=op)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
